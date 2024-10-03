a520a6817da59ef611394da2e4be1fbedf62011b
andrzejzelman <andrzejzelman@gmail.com>
2024-10-03 08:28:24 +0200

    Database setup, models, factories & seeders.

--- Modules/Tasks/routes/api.php
+++ Modules/Tasks/routes/api.php
@@ -14,6 +14,17 @@ use Modules\Tasks\Http\Controllers\TasksController;
  *
 */
 
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
+
     Route::apiResource('tasks', TasksController::class)->names('tasks');
 });
+
+//GET     /api/tasks                  # Get all tasks
+//GET     /api/tasks/{id}             # Get specific task by ID
+//POST    /api/tasks                  # Create a new task
+//PUT     /api/tasks/{id}             # Update a task
+//DELETE  /api/tasks/{id}             # Delete a task
+//GET     /api/users/{userId}/tasks   # Get all tasks for a user
+//POST    /api/tasks/{taskId}/assign  # Assign task to a user
+//PUT     /api/tasks/{taskId}/status  # Update task status
+//DELETE  /api/tasks/{taskId}/unassign# Unassign task from a user
